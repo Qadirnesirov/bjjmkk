1fe051353aecf5399e58320c484a5fb6c4a96bfc
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-03-03 17:33:59 +0400

    update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "AgC79hVl6TVbSiD6RZ-Imun9xuq7o6G5c_yesbCKsINn7Hxyi5YCOYyIk28DqO1aYIM0QPZi6cZkuzjB8q6Xd25hNPdnMI3x0f7IV3B9Y4oLJCeeHMa6vsw4qUM2WE_fQvWJXtyP2SFeCVK-34t3klcKljQ8wzDItjTxzeawwh74IODtMMmNTrwzfoof7BGvS5daMAIP9rGuUm-RRhEm9Yh2EwNiK4Cu1Qf0x1R3ptwfTIbbBsknEwb5HxPQauz5Q2XRy5Trl4N9UvEZqzX1iitPSmUsf2POY6jjdNpyqyw5MDtVTjENzXvWU0Kftr_4QeQrpgOfCvBZu6AF6cjR0G_CAAAAAVWiAQ0A")
+SESSION_NAME = getenv("SESSION_NAME", "Rahi Mall")
 BOT_TOKEN = getenv("BOT_TOKEN", "5910888289:AAF9HXtyMilAg4s4tl2W2xiXMIOOZbw-XWo")
 BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
 API_ID = int(getenv("API_ID", "8953338"))
